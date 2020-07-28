bbf1e97693a0ae2ddd3848f712c9c303cc56ed4c
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-28 13:41:02 +0300

    Initial Commit

---
+++ KeyboardResponder/KeyboardResponderApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardResponderApp.swift
+//  KeyboardResponder
+//
+//  Created by ramil on 28.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardResponderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
